fb990b541e2a411804b919a1a5217fa41e6c6a1e
qjoshlawlor <joshlawlor1@gmail.com>
2022-09-17 01:56:30 -0400

    change userSchema

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const SALT_ROUNDS = 6;
 
 const userSchema = new mongoose.Schema({
     username: String,
-    email: {type: String, required: true, lowercase: true, unique: true},
+    email: {type: String, required: false, lowercase: true, unique: true},
     password: {
         type: String,
         select: false,
